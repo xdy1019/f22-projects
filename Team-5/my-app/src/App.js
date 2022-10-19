d3f40b44869976c5b66e68c72f29445f714c557f
Dongyue Xu <xdy@bu.edu>
2022-10-19 16:03:03 -0400

    reupload code

--- Team-5/my-app/src/App.js
+++ Team-5/my-app/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Hello world.
+           Hello world.
         </p>
         
       </header>
